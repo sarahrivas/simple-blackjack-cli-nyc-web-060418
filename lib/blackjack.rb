aa09d26f55b4cb133ec23cb51c09a69b79e89c27
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-26 21:54:06 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,9 +57,9 @@ def runner
   welcome
   card_total = initial_round
     until card_total >= 21
-    end_game(card_total)
     card_total += hit?(card_total)
-  end
     display_card_total(card_total)
+    end
+  end_game(card_total)
 end
     
